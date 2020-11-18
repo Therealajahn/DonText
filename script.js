d1c16cf6972380eccde5fba7452669cf26fe55b8
Therealajahn <therealajahn@gmail.com>
2020-11-17 19:27:32 -0600

    text algorithm is wack, changing to type directly into div

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-let textInput = document.getElementByID("text-input");
-console.log(textInput);
+function putTextInADiv(){
+
+ let textBucket = document.getElementById('text-bucket');
+ 
+ let textDump = '';
+ let textPlate = document.getElementById('text-plate');
+    document.addEventListener('click',(e)=>{
+        textBucket.focus();
+
+        console.log('bucket focused');
+    });
+    textBucket.addEventListener('keydown',(e)=>{
+        
+        console.log()
+        textDump = e.target.value;
+        console.log('textDump', textDump);
+        textPlate.innerHTML = textDump;
+    });
+
+
+}
+
+putTextInADiv();
