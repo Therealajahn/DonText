4103e4118070c4e6791401e7e1bd54b39fadc4d6
Therealajahn <therealajahn@gmail.com>
2020-11-17 20:59:21 -0600

    write to disk emplemented, attempting read from disk

--- script.js
+++ script.js
@@ -1,23 +1,73 @@
-function putTextInADiv(){
 
- let textBucket = document.getElementById('text-bucket');
+
+function typeTextInADiv(){
  
- let textDump = '';
- let textPlate = document.getElementById('text-plate');
-    document.addEventListener('click',(e)=>{
-        textBucket.focus();
+    let textPlate = document.getElementById('text-plate');
+   
+        document.addEventListener('keydown',(e)=>{
+            
+            let keyCode = e.key.charCodeAt(0);
+            console.log('keyCode', keyCode);
 
-        console.log('bucket focused');
-    });
-    textBucket.addEventListener('keydown',(e)=>{
-        
-        console.log()
-        textDump = e.target.value;
-        console.log('textDump', textDump);
-        textPlate.innerHTML = textDump;
-    });
-
+            if(keyCode >= 32 && keyCode <= 127){
+                textPlate.innerHTML += e.key;
+            }
+            
+            //press Ctrl to save a file
+            if(e.key === "Control"){
+                saveFile(textPlate.innerHTML);
+            }
+            
+        });
 
 }
+typeTextInADiv();
 
-putTextInADiv();
+function saveFile(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
+function readSingleFile(e) {
+    var file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var contents = e.target.result;
+      displayContents(contents);
+    };
+    reader.readAsText(file);
+  }
+  
+  function displayContents(contents) {
+    var element = document.getElementById('file-content');
+    element.textContent = contents;
+  }
+  
+  document.getElementById('file-input')
+    .addEventListener('change', readSingleFile, false);
+
+// function specialKeys(key){
+//     if(key === 'Enter'){
+//         return '/n'
+//     }
+// }
+
+  function readLocalFile(){
+      
+  }
